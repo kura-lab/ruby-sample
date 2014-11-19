77a30e0d4c49c6702c6757f3897134f5f2879571
kura-lab <kmasaru86@gmail.com>
2014-11-20 01:47:58 +0900

    added how to install sqlite3

---
+++ sample06_sqlite3.rb
@@ -0,0 +1,29 @@
+#! ruby -Ku
+# -*- encoding: utf-8 -*-
+
+# SQLite3
+
+#
+# デフォルトのインストール先を確認
+# $ gem help install
+# ...
+# --install-dir
+#
+# SQLite3インストール
+# ビルドに必要なdevelインストール
+# $ sudo yum install sqlite-devel
+# $ gem install sqlite3-ruby
+# $ gem install dbi
+# $ gem install dbd-sqlite3
+#
+# インストール済みのライブラリ確認
+# $ gem list
+# 
+# *** LOCAL GEMS ***
+# 
+# dbd-sqlite3 (1.2.5)
+# dbi (0.4.5)
+# sqlite3 (1.3.10)
+# sqlite3-ruby (1.3.3)
+#
+
