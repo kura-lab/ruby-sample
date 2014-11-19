85a25e82c75c9bd71ef496d720cac1412259927a
kura-lab <kmasaru86@gmail.com>
2014-11-20 01:10:18 +0900

    added new sample of pstore

---
+++ sample05_pstore.rb
@@ -0,0 +1,40 @@
+#! ruby -Ku
+# -*- encoding: utf-8 -*-
+
+# PStore
+
+require 'pstore'
+
+db = PStore.new('bocchi.db')
+db.transaction do
+  # PStoreに保存
+  db["kura"] = "bocchi"
+  db["feeling"] = "sad"
+end # transactionをぬけるときに保存完了
+
+db.transaction do
+  # 読み込み
+  puts "kura is #{db["kura"]}"
+end
+
+# 引数にtrueで読み込み専用
+db.transaction(true) do
+  # 書き込みはエラー
+  # db["kura"] = "not bocchi"
+  
+  # rootsで全キー取得
+  db.roots.each { |key|
+    puts "key: #{key}"
+  }
+
+end
+
+db.transaction do
+  # 削除
+  if db.root?("kura")
+    db.delete("kura")
+    puts "kura is deleted"
+  end
+end
+
+File.delete("bocchi.db")
