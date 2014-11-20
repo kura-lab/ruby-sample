c46cda1d83a6aa9a70c1b825e9eb0b4b10b545ff
kura-lab <kmasaru86@gmail.com>
2014-11-20 23:23:01 +0900

    added new sample of sqlite3

--- sample06_sqlite3.rb
+++ sample06_sqlite3.rb
@@ -27,3 +27,48 @@
 # sqlite3-ruby (1.3.3)
 #
 
+require 'rubygems' # gemsでインストール時には必要
+require 'dbi'
+
+dbh = DBI.connect('DBI:SQLite3:bocchi.db')
+
+# 既存テーブルの削除
+dbh.do("drop table if exists members")
+puts "table 'members' was dropped."
+
+# 新規テーブルの作成
+dbh.do("create table members (
+        id   int          not null,
+        name varchar(100) not null,
+        age  int          not null,
+        jod  varchar(100) not null,
+        primary key(id));")
+puts "table 'members' was created."
+
+# データの挿入
+dbh.do("insert into members values (
+        1,
+        'kura',
+        18,
+        'web engineer');")
+dbh.do("insert into members values (
+        2,
+        'kurara',
+        18,
+        'web designer');")
+puts "2 members inserted."
+
+# データの読み込み
+sth = dbh.execute("select * from members")
+sth.each do |row|
+  row.each_with_name do |val, name|
+    puts "#{name}: #{val.to_s}"
+  end
+end
+sth.finish
+
+dbh.execute("delete from members")
+puts "members table deleted."
+
+# 接続終了
+dbh.disconnect
