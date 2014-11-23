e203128c1f4a447a9505b62c8d58ae08ca7cf5a3
kura-lab <kmasaru86@gmail.com>
2014-11-24 03:21:19 +0900

    added new sample of rsa

---
+++ scripts/sample15_rsa.rb
@@ -0,0 +1,26 @@
+#! ruby -Ku
+# -*- encoding: utf-8 -*-
+
+# RSA
+
+require "base64"
+require "openssl"
+include OpenSSL::PKey
+
+rsa = RSA.generate(2048)
+puts rsa
+puts rsa.public? # 公開鍵が含まれるかの有無
+puts rsa.private? # 秘密鍵が含まれるかの有無
+
+publicKey = rsa.public_key.to_s
+
+pub = RSA.new(publicKey)
+encrypted = pub.public_encrypt("kura is bocchi")
+puts "encrypted: " + Base64.encode64(encrypted)
+
+# 秘密鍵の暗号化なし
+# privateKey = rsa.export()
+privateKey = rsa.export(OpenSSL::Cipher::Cipher.new("aes256"),"passwd")
+pri = RSA.new(privateKey)
+decrypted = pri.private_decrypt(encrypted)
+puts "decrypted: " + decrypted
